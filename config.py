d3b8daa170858ef2132dd65a72baff5c49b3236c
gokul2007kannan <157709328+gokul2007kannan@users.noreply.github.com>
2024-03-03 15:39:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,23 +2,23 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "20244111")
+API_ID = os.environ.get("API_ID", "12323023")
 
-API_HASH = os.environ.get("API_HASH", "b76d27da2a4220fe109fe9ef0e866530")
+API_HASH = os.environ.get("API_HASH", "26f4cc59ffcc290ef279958b49ea37ed")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6132728857:AAHX16VnWQl1fqto91_7M4H8eQ6IxCkI9cs") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7137889955:AAG0rWTqxrZkze_I8P9g7X7rfuSGCr7X0ms") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "https://t.me/xadmin_bots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "https://t.me/Team_TRL") 
 
 DB_NAME = os.environ.get("DB_NAME","TELEGRAM FILES")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://kfakeid9:Jobijobi1@musicdb.urlvere.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://gokul2007kannan:<zhx6zZf6wldvdCFY>@cluster0.qos2zcj.mongodb.net/")
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = int(os.environ.get("FLOOD", "1"))
 
-START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/aa1faf8dafa22f4a9ce48.jpg")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/5f5d06dca1f0acf3603ad.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5572938538').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5326845651').split()]
 
 PORT = os.environ.get('PORT', '8080')
 
